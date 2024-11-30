852ad26dec561f14b80e8aeef36bd76cd603e79f
pyojeyn <jeynpyo@gmail.com>
2024-11-30 14:57:40 +0900

    배열 2배 만들기!

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -7,23 +7,21 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
-// 머쓱이보다 키 큰 사람
-/* 머쓱이네 반 친구들의 키가 담긴 정수 배열 array와
-머쓱이의 키height가 매개변수로 주어질 때,
-머쓱이보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요. */
+// 배열 2배 만들기!
+/*
+ * 정수 배열 numbers 가 매개변수.
+ * numbers 의 각 원소에 두배한 원소를 가진 배열을 return 하도록 solution 함수를 완성하기.
+ * */
 public class Solution1 {
     public static void main(String[] args) {
-        int[] classFHeights = {149, 180, 192, 170};
-
-        System.out.println(solution(classFHeights, 167));
+        int[] arr = {1, 4, 6, 7};
+        System.out.println(Arrays.toString(solution(arr)));
     }
 
-    public static int solution(int[] array, int height){
-        int answer = 0;
+    public static int[] solution(int[] numbers){
 
-        answer = (int) Arrays.stream(array)
-                .filter(a -> a > height).count();
-
+        int[] answer = Arrays.stream(numbers)
+                .map(i -> i*2).toArray();
         return answer;
     }
 }

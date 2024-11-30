bc5be06fab6d706e858addb37ff93f3f7d41bef2
pyojeyn <jeynpyo@gmail.com>
2024-11-30 21:21:11 +0900

    문자열 정렬하기 (1)

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -7,28 +7,29 @@ import java.util.stream.LongStream;
 
 
 /*
-    [순서쌍의 개수]
-    순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
-    자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를
-    return하도록 solution 함수를 완성해주세요.
+    [문자열 정렬하기 (1)]
+    문자열 my_string이 매개변수로 주어질 때,
+    my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
  */
 public class Solution1 {
     public static void main(String[] args) {
 
-        System.out.println(solution(100));
+        System.out.println(Arrays.toString(solution("p2o4i8gj2")));
 
 
     }
 
-    public static int solution(int n){
-        int cnt = 0;
+    public static int[] solution(String my_string){
+        String[] newMyString2 = my_string.replaceAll("[^0-9]", "").split("");
+        int[] answer = new int[newMyString2.length];
 
-        for (int i=1; i<=n; i++){
-            if(n%i == 0){
-                cnt++;
-            }
+        for(int i=0; i<newMyString2.length; i++){
+           answer[i] = Integer.parseInt(newMyString2[i]);
         }
 
-        return cnt;
+        Arrays.sort(answer);
+
+       return answer;
+
     }
 }

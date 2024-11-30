49a9deb917ddb85f2fe33b19db05599468aafc14
pyojeyn <jeynpyo@gmail.com>
2024-11-30 14:58:43 +0900

    피자 나눠먹기1,3

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -7,21 +7,23 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
-// 배열 2배 만들기!
-/*
- * 정수 배열 numbers 가 매개변수.
- * numbers 의 각 원소에 두배한 원소를 가진 배열을 return 하도록 solution 함수를 완성하기.
- * */
+// 피자 나눠먹기
 public class Solution1 {
     public static void main(String[] args) {
-        int[] arr = {1, 4, 6, 7};
-        System.out.println(Arrays.toString(solution(arr)));
+        System.out.println(solution_1(4,12));
     }
 
-    public static int[] solution(int[] numbers){
-
-        int[] answer = Arrays.stream(numbers)
-                .map(i -> i*2).toArray();
+    // 피자 나눠먹기1
+    public static int solution(int n) {
+        double pizza = (double) n / 7;
+        System.out.println(pizza);
+        int answer = (int) Math.ceil(pizza);
         return answer;
     }
+
+    // 피자 나눠먹기 3
+    public static int solution_1(int slice, int n) {
+        return (int) Math.ceil((double) n/slice);
+    }
+
 }

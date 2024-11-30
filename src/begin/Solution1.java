e63c14a963ff31db4ecdbfecc20d51ecd8c4ac83
pyojeyn <jeynpyo@gmail.com>
2024-11-30 15:12:49 +0900

    약수구하기

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -5,30 +5,29 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
-// 직각삼각형 출력.
 /*
-"*"의 높이와 너비를 1이라고 했을 때, "*"을 이용해 직각 이등변 삼각형을 그리려고합니다.
-정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
- */
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+약수구하기
+* 정수 n이 매개변수로 주어질 때,
+* n의 약수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+* */
 public class Solution1 {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(24)));
+    }
+
+    public static int[] solution(int n){
+        List<Integer> tmpList = new ArrayList<>();
 
 
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-        int n = 3;
-
-        for (int i = 1; i <= n; i++) { // i는 현재 줄 번호
-            for (int j = 1; j <= i; j++) { // j는 해당 줄에서 출력할 '*'의 개수
-                System.out.print("*");
+        for(int i=1; i<=n; i++){
+            if(n % i == 0){
+                tmpList.add(i);
             }
-            System.out.println(); // 줄 바꿈
         }
+        int[] answer = tmpList.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(answer);
+
+        return  answer;
     }
 }

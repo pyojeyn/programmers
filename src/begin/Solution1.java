33ce9417951aa08fa9c713f334574fd4b9487453
pyojeyn <jeynpyo@gmail.com>
2024-11-30 21:10:11 +0900

    순서쌍의 개수

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -7,28 +7,28 @@ import java.util.stream.LongStream;
 
 
 /*
-    [삼각형의 완성조건]
-    선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
-
-    * 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
-    삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다.
-    세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
+    [순서쌍의 개수]
+    순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
+    자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를
+    return하도록 solution 함수를 완성해주세요.
  */
 public class Solution1 {
     public static void main(String[] args) {
 
-
+        System.out.println(solution(100));
 
 
     }
 
-    public static int solution(int[] sides){
-        int answer = 0;
+    public static int solution(int n){
+        int cnt = 0;
 
-        Arrays.sort(sides);
-        int longest = sides[2];
-        answer = longest < sides[0] + sides[1] ? 1 : 2;
+        for (int i=1; i<=n; i++){
+            if(n%i == 0){
+                cnt++;
+            }
+        }
 
-        return answer;
+        return cnt;
     }
 }

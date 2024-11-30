a523d26c2548fd5142a5cb6bf26d2a3bcbb67b9c
pyojeyn <jeynpyo@gmail.com>
2024-11-30 14:54:56 +0900

    짝수 홀수 개수 구하기

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -4,37 +4,37 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
-// 양꼬치
+// 짝수 홀수 개수 구하기
 public class Solution1 {
     public static void main(String[] args) {
-        int howMuch = solution(64, 6);
-
-        System.out.println("howMuch?" + howMuch);
-        int a = 9;
-        int b = 10;
-        System.out.println(a/b);
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(solution(arr)));
+        System.out.println(Arrays.toString(solution_1(arr)));
     }
-    public static int solution(int n, int k) {
-        // n : 양꼬치
-        // k : 음료수
-        // 10인분 먹었을 시 음료수1개 꽁짜.
-        int answer = 0;
-
-        // 내가 푼 것.
-//        if(n >= 10){
-//            int howFreeDrink = n / 10;
-//            answer =  (12000 * n) + (2000 * (k - howFreeDrink));
-//        }else{
-//            answer = (12000 * n) + (2000 * k);
-//        }
-
-        // 개선 -> 정수 나눗셈이므로 10보다 작은수를 10으로 나누면 0 이 되므로.
-        // 아래처럼 if문 필요 없이 바로 10으로 나눠서 반환해주면 된다.
-        answer = 12000 * n + (k - n/10) * 2000;
 
 
-        return answer;
+    // mine
+    public static int[] solution(int[] num_list){
+        int evenCnt = 0;
+        int oddCnt = 0;
+        for(int i=0; i<num_list.length; i++){
+            int num = num_list[i];
+            if(num % 2 ==0){
+                evenCnt++;
+            }else{
+                oddCnt++;
+            }
+        }
+
+        return new int[]{evenCnt, oddCnt};
+    }
+
+    // others
+    public static int[] solution_1(int[] num_list){
+        return IntStream.of((int) Arrays.stream(num_list).filter(i -> i % 2==0).count(),
+                (int) Arrays.stream(num_list).filter(i -> i % 2 ==1).count()).toArray();
     }
 }

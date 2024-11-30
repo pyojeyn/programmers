d08b94e1eb18e89ccc3ae87e271db6906270610b
pyojeyn <jeynpyo@gmail.com>
2024-11-30 21:04:14 +0900

    삼각형의 완성조건

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -7,35 +7,28 @@ import java.util.stream.LongStream;
 
 
 /*
-    최대값 만들기(2)
-    정수 배열 numbers가 매개변수로 주어집니다.
-    numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+    [삼각형의 완성조건]
+    선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+
+    * 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+    삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다.
+    세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
  */
 public class Solution1 {
     public static void main(String[] args) {
 
-        System.out.println(solution(new int[]{0, 0, 1, 0, 9}));
-//        System.out.println(solution(new int[]{-20, 1, 1, 4, -5}));
-//        System.out.println(solution(new int[]{1, 2, -3, 4, -5}));
-//        System.out.println(solution(new int[]{0, -31, 24, 10, 1, 9}));
-//        System.out.println(solution(new int[]{10, 20, 30, 5, 5, 20, 5}));
+
 
 
     }
 
-    public static int solution(int[] numbers){
+    public static int solution(int[] sides){
         int answer = 0;
 
-        // ** 정렬이 가능한지 먼저 떠올리기.
-        // 정렬.
-        Arrays.sort(numbers);
+        Arrays.sort(sides);
+        int longest = sides[2];
+        answer = longest < sides[0] + sides[1] ? 1 : 2;
 
-        // 가장 큰 두 양수의 곱
-        int pMax = numbers[numbers.length -1] * numbers[numbers.length -2];
-        // 가장 작은 두 음수의 곱
-        int nMax = numbers[0] * numbers[1];
-
-        answer = Math.max(pMax, nMax);
         return answer;
     }
 }

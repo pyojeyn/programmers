7b76042d699dba3ba2e2cc4cdc8d7645d87ade3d
pyojeyn <jeynpyo@gmail.com>
2024-11-30 15:10:27 +0900

    직각삼각형 출력

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -5,26 +5,30 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
-// n 의 배수 고르기.
-// 정수 n과 정수 배열 numlist가 매개변수로 주어질 때,
-// numlist에서 n의 배수가 아닌 수들을 제거한 배열을 return하도록
-// solution 함수를 완성해주세요.
+// 직각삼각형 출력.
+/*
+"*"의 높이와 너비를 1이라고 했을 때, "*"을 이용해 직각 이등변 삼각형을 그리려고합니다.
+정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Solution1 {
 
-
-    /*
-    n	numlist	                        result
-    3	[4, 5, 6, 7, 8, 9, 10, 11, 12]	[6, 9, 12]
-     */
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(3, new int[]{4, 5, 6, 7, 8, 9, 10, 11, 12})));
-    }
-    public static int[] solution(int n, int[] numlist){
 
-        int[] answer = Arrays.stream(numlist)
-                .filter(num -> num % n == 0)
-                .toArray();
 
-        return answer;
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+        int n = 3;
+
+        for (int i = 1; i <= n; i++) { // i는 현재 줄 번호
+            for (int j = 1; j <= i; j++) { // j는 해당 줄에서 출력할 '*'의 개수
+                System.out.print("*");
+            }
+            System.out.println(); // 줄 바꿈
+        }
     }
 }

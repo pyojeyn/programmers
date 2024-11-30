c00ccf4d36798f33cc8893c41641163840df7d5c
pyojeyn <jeynpyo@gmail.com>
2024-11-30 15:18:08 +0900

    인덱스 바꾸기

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,42 +6,32 @@ import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 
-// 제곱수 판별하기
-// 어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다.
-// 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록
-// solution 함수를 완성해주세요.
+/*
+ * 인덱스 바꾸기
+ * 문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서
+ * 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록
+ * solution 함수를 완성해보세요.
+ * */
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution(27));
-        System.out.println(solution2(27));
+//        System.out.println(solution("AbcAbcA", "AAA"));
+        System.out.println(changeIndex("hello", 1, 2));
+
     }
 
-    // 다른 사람 풀이
-    public static int solution2(int n) {
-        for(int i=0; i<= 1000; i++) {
-            if(i*i == n )
-                return 1;
-        }
-        return 2;
+    public static int solution(String str1, String str2){
+        return str1.contains(str2) ? 1 : 2;
     }
 
-    // 나의 풀이..
-    //  핑계를 대자면.. 거듭제곱도 같이 생각해서 코드가 길어졌다...
-    public static int solution(int n){
-        int answer = 2;
+    public static String changeIndex(String my_string, int num1, int num2){
+        String answer = "";
+        char[] charArr = my_string.toCharArray();
 
-        List<Integer> tmp = new ArrayList<>();
-        for(int i=1; i<=n; i++){
-            if(n%i == 0){
-                tmp.add(i);
-            }
-        }
 
-        for (Integer num : tmp) {
-            if (num*num == n){
-                answer = 1;
-            }
-        }
+        charArr[num1] = my_string.charAt(num2);
+        charArr[num2] = my_string.charAt(num1);
+
+        answer = String.valueOf(charArr);
 
         return answer;
     }

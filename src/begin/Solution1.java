1933562aefa8254a14661ea723bec0c85771b677
pyojeyn <jeynpyo@gmail.com>
2024-11-30 15:02:57 +0900

    개미군단

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -7,34 +7,60 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
-/*
-최대값 만들기(1)
-* 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중
-* 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
-* */
+// 개미군단
 public class Solution1 {
-    public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
 
-        System.out.println(solution(numbers));
+    public static void main(String[] args) {
+        System.out.println(solution(23));
+        System.out.println(solution_1(23));
     }
 
-    public static int solution(int[] numbers){
+    // 5분 남기고 푼... 나의 지저분한.. 코드....
+    public static int solution(int hp) {
         int answer = 0;
-        // 참조형 배열 생성
-        Integer[] numbersWrapper = new Integer[numbers.length];
 
-        // 참조형 배열 초기화
-        for(int i=0; i<numbers.length; i++){
-            numbersWrapper[i] = numbers[i];
+        if(hp / 5 > 0){
+            answer += (hp / 5);
+            if (hp % 5 >= 3){
+                int aa = hp%5;
+                answer += (aa / 3);
+                if(aa % 3 > 0 ){
+                    int bb = aa%3;
+                    answer += bb;
+                }
+            }else{
+                answer += hp%5;
+            }
+        }else if(hp / 3 > 0){
+            answer += (hp / 3);
+            int aa  = hp%3;
+            if(aa >= 1){
+                answer += hp%3;
+            }
+        }else{
+            answer += hp;
         }
 
+        return answer;
+    }
 
-        // 내림차순 정렬
-        Arrays.sort(numbersWrapper, Collections.reverseOrder());
 
-        // 곱함.
-        answer = numbersWrapper[0] * numbersWrapper[1];
+    // more simple
+    // 큰 단위부터 차례대로 나눠주면 되는 문제...
+    public static int solution_1(int hp){
+        int answer = 0;
+
+        // 장군 개미 수 계산
+        answer += hp/5;
+        hp %= 5;
+
+        // 병정 개미 수 계산
+        answer += hp/3;
+        hp%=3;
+
+        // 일개미 수 계산
+        answer += hp;
+
         return answer;
     }
 }

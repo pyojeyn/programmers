5aa3aeefa1dec2ab41697015dd94bf419f1123d0
pyojeyn <jeynpyo@gmail.com>
2024-11-30 14:55:44 +0900

    배열의 유사도

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -7,34 +7,20 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
-// 짝수 홀수 개수 구하기
+// 배열의 유사도
 public class Solution1 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(solution(arr)));
-        System.out.println(Arrays.toString(solution_1(arr)));
+        String[] s1 = {"n", "omg"};
+        String[] s2 = {"m", "dot"};
+
+        System.out.println(solution(s1, s2));
+
     }
 
+    public static int solution(String[] s1, String[] s2) {
+        int answer = 0;
 
-    // mine
-    public static int[] solution(int[] num_list){
-        int evenCnt = 0;
-        int oddCnt = 0;
-        for(int i=0; i<num_list.length; i++){
-            int num = num_list[i];
-            if(num % 2 ==0){
-                evenCnt++;
-            }else{
-                oddCnt++;
-            }
-        }
-
-        return new int[]{evenCnt, oddCnt};
-    }
-
-    // others
-    public static int[] solution_1(int[] num_list){
-        return IntStream.of((int) Arrays.stream(num_list).filter(i -> i % 2==0).count(),
-                (int) Arrays.stream(num_list).filter(i -> i % 2 ==1).count()).toArray();
+        answer = (int) Arrays.stream(s1).filter(e -> Arrays.stream(s2).anyMatch(e::equals)).count();
+        return answer;
     }
 }

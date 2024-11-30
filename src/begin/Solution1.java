92b4645876a4f1f03d671a7a3bca62b13d4414aa
pyojeyn <jeynpyo@gmail.com>
2024-11-30 22:11:05 +0900

    피자 나눠먹기(2)

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -7,27 +7,27 @@ import java.util.stream.LongStream;
 
 
 /*
-    [문자열 정렬하기 (1)]
-    문자열 my_string이 매개변수로 주어질 때,
-    my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
+    [피자 나눠먹기(2)]
+    머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때,
+    n명이 주문한 피자를 남기지 않고 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
  */
 public class Solution1 {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(solution("p2o4i8gj2")));
+        System.out.println(solution(10));
 
 
     }
 
-    public static int[] solution(String my_string){
-        String[] newMyString2 = my_string.replaceAll("[^0-9]", "").split("");
-        int[] answer = new int[newMyString2.length];
+    public static int solution(int n){
+       int answer = 0;
 
-        for(int i=0; i<newMyString2.length; i++){
-           answer[i] = Integer.parseInt(newMyString2[i]);
-        }
-
-        Arrays.sort(answer);
+       for(int i=1; i<=6; i++){
+           if(6*i %n == 0){
+               answer = i;
+               break;
+           }
+       }
 
        return answer;
 

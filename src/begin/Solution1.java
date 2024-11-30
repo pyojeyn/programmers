e440eefd9e0f4d3e96c57e64537ad3bc3cc8409e
pyojeyn <jeynpyo@gmail.com>
2024-11-30 15:03:28 +0900

    가위 바위 보

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -1,66 +1,61 @@
 package begin;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
-// 개미군단
+// 가위 바위 보.
+/*
+ * 가위는 2 바위는 0 보는 5로 표현합니다.
+ * 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때,
+ * rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록
+ * solution 함수를 완성해보세요.
+ * */
 public class Solution1 {
 
-    public static void main(String[] args) {
-        System.out.println(solution(23));
-        System.out.println(solution_1(23));
+
+    // 나의 답....
+    public static String solution(String rsp){
+        String win2 = "0";
+        String win0 = "5";
+        String win5 = "2";
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<rsp.length(); i++){
+            String compareStr = String.valueOf(rsp.charAt(i));
+
+            if(compareStr.equals("2")){
+                sb.append(win2);
+            }
+
+            if(compareStr.equals("0")){
+                sb.append(win0);
+            }
+
+            if(compareStr.equals("5")){
+                sb.append(win5);
+            }
+        }
+        return sb.toString();
     }
 
-    // 5분 남기고 푼... 나의 지저분한.. 코드....
-    public static int solution(int hp) {
-        int answer = 0;
 
-        if(hp / 5 > 0){
-            answer += (hp / 5);
-            if (hp % 5 >= 3){
-                int aa = hp%5;
-                answer += (aa / 3);
-                if(aa % 3 > 0 ){
-                    int bb = aa%3;
-                    answer += bb;
-                }
-            }else{
-                answer += hp%5;
-            }
-        }else if(hp / 3 > 0){
-            answer += (hp / 3);
-            int aa  = hp%3;
-            if(aa >= 1){
-                answer += hp%3;
-            }
-        }else{
-            answer += hp;
+    // 다른 사람이 푼 답.
+    // Map 을 사용할 수 있다.
+    public static String solution_1(String rsp){
+        Map<String, String> winNumbers = new HashMap<>();
+        winNumbers.put("2", "0");
+        winNumbers.put("0", "5");
+        winNumbers.put("5", "2");
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i =0; i <rsp.length(); i++){
+            stringBuilder.append(winNumbers.get(rsp.substring(i, i + 1)));
         }
 
-        return answer;
-    }
 
-
-    // more simple
-    // 큰 단위부터 차례대로 나눠주면 되는 문제...
-    public static int solution_1(int hp){
-        int answer = 0;
-
-        // 장군 개미 수 계산
-        answer += hp/5;
-        hp %= 5;
-
-        // 병정 개미 수 계산
-        answer += hp/3;
-        hp%=3;
-
-        // 일개미 수 계산
-        answer += hp;
-
-        return answer;
+        return stringBuilder.toString();
     }
 }

0ee058346d2b746dd15585d4e332e20c73318174
pyojeyn <jeynpyo@gmail.com>
2024-12-01 18:29:48 +0900

    문자열 정렬하기

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -7,35 +7,23 @@ import java.util.stream.LongStream;
 
 
 /*
-    숫자 찾기
-    정수 num과 k가 매개변수로 주어질 때,
-    num을 이루는 숫자 중에 k가 있으면 num의 그 숫자가 있는 자리 수를 return하고
-    없으면 -1을 return 하도록 solution 함수를 완성해보세요.
+    [문자열 정렬하기]
+    영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때,
+    my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
  */
-// 단번에 통과
+// 1점 획득. 단번에 통과.
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution(29183, 1));
-        System.out.println(solution(232443, 4));
-        System.out.println(solution(123456, 7));
-
+        System.out.println(solution("Bcad"));
+        System.out.println(solution("heLLo"));
+        System.out.println(solution("Python"));
     }
 
-    public static int solution(int num, int k){
-        int answer = -1;
-        String numStr = num + "";
-        String[] arr = numStr.split("");
-
-        for(int i=0; i<numStr.length(); i++){
-            int iNum = Integer.parseInt(arr[i]);
-            if(iNum == k){
-                answer = i + 1;
-                break;
-            }
-        }
-
-
-        return answer;
+    public static String solution(String my_string){
+        String lowerCase = my_string.toLowerCase();
+        char[] lowerCaseCharArr = lowerCase.toCharArray();
+        Arrays.sort(lowerCaseCharArr); //  ASCII 값 순서로 정렬
+        return String.valueOf(lowerCaseCharArr);
     }
 
 

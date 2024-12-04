f13ef6763f9ce610fe1347dea32ed3307c2adc8b
pyojeyn <jeynpyo@gmail.com>
2024-12-04 21:01:07 +0900

    진료 순서 정하기

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -1,33 +1,47 @@
 package begin;
 
 import java.util.*;
+import java.util.concurrent.Semaphore;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 
 /*
-    [k의 개수]
-    1부터 13까지의 수에서, 1은 1, 10, 11, 12, 13 이렇게 총 6번 등장합니다.
-    정수 i, j, k가 매개변수로 주어질 때,
-    i부터 j까지 k가 몇 번 등장하는지 return 하도록 solution 함수를 완성해주세요.
+    [진료 순서 정하기]
+    외과의사 머쓱이는 응급실에 온 환자의 응급도를 기준으로 진료 순서를 정하려고 합니다.
+    정수 배열 emergency가 매개변수로 주어질 때 응급도가 높은 순서대로 진료 순서를 정한 배열을 return하도록 solution 함수를 완성해주세요.
  */
-// 4점 획득. 단번에 통과!
+
+// 1점 획득.
+
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution(1, 13, 1));
-        System.out.println(solution(10, 50, 5));
-        System.out.println(solution(3, 10, 2));
+        System.out.println(Arrays.toString(solution(new int[]{3, 76, 24})));
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 4, 5, 6, 7})));
+        System.out.println(Arrays.toString(solution(new int[]{30, 10, 23, 6, 100})));
     }
 
-    public static int solution (int i, int j, int k){
-        int answer = 0;
-        for(int num=i; num<=j; num++){
-            int count = (int) Arrays.stream((num + "").split(""))
-                    .filter(x -> x.contains(k + "")).count();
-            answer += count;
+    public static int[] solution (int[] emergency){
+
+        // 기존 배열을 정렬해서 새로운 배열 만듬.
+        int[] tmpArr = IntStream.of(emergency)
+                .boxed()
+                .sorted(Collections.reverseOrder()) // Collections.reverseOrder()와 같은 메서드는 객체(Integer) 타입에서만 사용할 수 있기 때문에 boxed() 메서드 사용
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        // 순위 Map 을 만듬
+        Map<Integer, Integer> rankMap = new HashMap<>();
+        for(int i=0; i< emergency.length; i++){
+            rankMap.put(tmpArr[i], i + 1);
         }
-        return answer;
+
+        for(int i=0; i< emergency.length; i++){
+            emergency[i] = rankMap.get(emergency[i]);
+        }
+
+        return emergency;
     }
 
 

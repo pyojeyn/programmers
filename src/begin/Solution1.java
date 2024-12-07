148db8207ebd1ca3b505511ff2717888a16fd5f3
pyojeyn <jeynpyo@gmail.com>
2024-12-07 21:53:30 +0900

    이진수 더하기

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -5,42 +5,20 @@ import java.util.stream.Collectors;
 
 
 /*
-    [소인수분해]
-     소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다.
-     예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다.
-     따라서 12의 소인수는 2와 3입니다.
-     자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을
-     return 하도록 solution 함수를 완성해주세요.
+    [이진수 더하기]
+     이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때,
+     두 이진수의 합을 return 하도록 solution 함수를 완성해주세요.
  */
-
-// OH!!!!!!!!!!!!!!!!!!!!!!!!!! 9점 획득입니다!
-// 수학관련이라 어려웠는데 뿌듯하다.
+// Oh... 7점???
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution( 17)));
+        System.out.println(solution("10", "11"));
     }
 
 
-    public static int[] solution(int n){
-
-        // 개선점. 중복제거니까 List 를 안쓰고 Set 을 써도 됬다. 그러머 distinct 도 필요없다.
-        List<Integer> soinsuList = new ArrayList<>();
-
-        // 소수는 2부터 시작.
-        int sosu = 2;
-
-        // n 이 1이 될때까지 무한 반복문.
-        while (!(n == 1)){
-            // 매개변수와 소수의 나머지가 0으로 떨어질때.
-            if(n%sosu==0){
-                // 소수를 리스트에 추가
-                soinsuList.add(sosu);
-                // n을 소수로 나눈 값으로 대입.
-                n = n/sosu;
-            }else sosu++; // 매개변수와 소수의 나머지가 0으로떨어질때까지 소수의 값을 1씩 늘려줌.
-
-        }
-        return soinsuList.stream().distinct().mapToInt(Integer::intValue).toArray();
+    public static String solution(String bin1, String bin2){
+        int sum = Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2);
+        return Integer.toBinaryString(sum);
     }
 
 }

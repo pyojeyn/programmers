e6fb94065db1aba251f3d7bf4e18a3d7cd86fcb0
pyojeyn <jeynpyo@gmail.com>
2024-12-08 22:43:41 +0900

    [공 던지기] - 배열 순환할때 나머지 연산으로 나온 값이 인덱스 값이 된다.

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -2,58 +2,65 @@ package begin;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 /*
-    [잘라서 배열로 저장하기]
-     문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+    [공 던지기]
+     머쓱이는 친구들과 동그랗게 서서 공 던지기 게임을 하고 있습니다.
+     공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 그다음 사람에게만 던질 수 있습니다.
+     친구들의 번호가 들어있는 정수 배열 numbers와 정수 K가 주어질 때,
+     k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 solution 함수를 완성해보세요.
+     numbers의 길이는 3부터 99까지.
+     k의 길이는 1부터 999까지.
  */
-// 2점 획득!
+// 5점 획득! 어려웠지만.. 어찌저찌 풀어서 5점 받았다.
 public class Solution1 {
     public static void main(String[] args) {
-         System.out.println(Arrays.toString(solution("abc1Addfggg4556b", 6)));
-         // (0,6) (6, 12) (12)
-         System.out.println(Arrays.toString(solution("abcdef123", 3)));
-         // (0,3) (3, 6) (6)
-        System.out.println("---------------------------------------------------------------------------------");
-        System.out.println(Arrays.toString(solution_1("abc1Addfggg4556b", 6)));
-        System.out.println(Arrays.toString(solution_1("abcdef123", 3)));
+        // [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
+        // [1,2,3,1,2,3]
+        System.out.println(solution_1(new int[]{1,2,3,4}, 2));
+        System.out.println(solution_1(new int[]{1, 2, 3, 4, 5, 6}, 5));
+        System.out.println(solution_1(new int[]{1, 2, 3}, 3));
     }
 
 
-    public static String[] solution(String my_str, int n){
-        int ceil = (int) Math.ceil(my_str.length() / (double) n);
-        String[] answer = new String[ceil];
-        String substring = "";
+    public static int solution(int[] numbers, int k){
+        int answer = 0;
+        int sd = numbers.length / 2;
+        int[] numbersPlus = {};
 
-        // for문 초기식에서 여러 변수 선언. i, answerIndex
-        for(int i=0, answerIndex=0; i<my_str.length(); i+=n, answerIndex++){
-            // 개선. 삼항연산자로 깔끔하게 하자.
-//            if(i+n < my_str.length()){
-//                substring = my_str.substring(i, i + n);
-//            }else{
-//                substring = my_str.substring(i);
-//            }
-//            answer[answerIndex] = substring;
-
-            answer[answerIndex] = (i + n < my_str.length()) ? my_str.substring(i, i + n) : my_str.substring(i);
+        // 진짜 비효율적임 굳이 배열을 확장할 필요가 없었다..
+        // 괜찮어 배열 인덱스 순환할줄 몰라서 그런거니까...!!!
+        if(sd <= k){
+            numbersPlus = IntStream.range(0, k)
+                    .flatMap(i -> IntStream.of(numbers)) // 배열을 times 번 반복
+                    .toArray();// 최종 결과 배열로 변환
         }
-        return answer;
-    }
 
-    // 처음 문제를 풀때 for 문에서 i가 n의 배수만큼 늘어나면 i를 answer의 인덱스에 대입을 못하는 고민이 생겨서 결국 위의 답을 했지만,
-    // 다른 사람의 풀이를 보고 깨달았다..
-    // i는 그대로 순차적으로 1씩 증가시키고, substring 의 인자1은 i*n, 인자2는 i*n + n / 온리 인자 i*n 이라는것을....
-    // 이런식으로 하면 굳이 for문에 answerIndex 같은 변수를 선언 안해줘도 되는데...
-    public static String[] solution_1(String my_str, int n){
-        int ceil = (int) Math.ceil(my_str.length() / (double) n);
-        String[] answer = new String[ceil];
-
-        for(int i=0; i< answer.length; i++){
-            answer[i] = (i * n + n < my_str.length()) ? my_str.substring(i * n, i * n + n) : my_str.substring(i * n);
+        int cnt = 1;
+        for (int i=0; i<numbersPlus.length; i+=2){
+            if(cnt == k){
+                answer = numbersPlus[i];
+                break;
+            }
+            cnt++;
         }
 
         return answer;
     }
+
+    public static int solution_1(int[] numbers, int k){
+        // 2 % 4 = 2번 인덱스
+        // 8 % 6 = 2번 인덱스
+        // 4 % 3 = 1번 인덱스
+
+        int index = (2 * (k - 1)) % numbers.length;
+        /* k - 1: 공을 첫 번째로 던지는 사람은 이미 선택(맨첫번째)되었기 때문에, k-1번의 이동이 필요하다.
+           2 * (k - 1) : 한 번에 두 칸씩 건너뛰므로. 총 이동거리는 저렇게 된다.
+           % numbers.length : 나머지값이 인덱스가 됨. 나머지 연산으로 배열의 인덱스를 순환시킬 수 있음. */
+        return numbers[index];
+    }
+
 
 }

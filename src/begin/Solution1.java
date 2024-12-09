a287689730a27598d1b2bdef431d71c6644ea90f
pyojeyn <jeynpyo@gmail.com>
2024-12-09 18:00:11 +0900

    영어가 싫어요

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,49 +6,64 @@ import java.util.stream.IntStream;
 
 
 /*
-    [문자열 계산하기]
-     my_string은 "3 + 5"처럼 문자열로 된 수식입니다. 문자열 my_string이 매개변수로 주어질 때,
-     수식을 계산한 값을 return 하는 solution 함수를 완성해주세요.
+    [영어가 싫어요]
+     영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다.
+     문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
  */
-// 11점!!!!!!!
+
+
+// 4점.
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution("3 + 5 - 2"));
-        System.out.println(solution_1("3 + 5 - 2"));
+        System.out.println(solution("onetwothreefourfivesixseveneightnine"));
+        System.out.println(solution("onefourzerosixseven"));
     }
 
 
-    public static int solution(String my_string){
-        String[] myStringArr = my_string.split(" ");
+    public static long solution(String numbers){
+        Map<String, String> numbersMap = Map.of(
+                "zero","0",
+                "one", "1",
+                "two", "2",
+                "three", "3",
+                "four", "4",
+                "five", "5",
+                "six", "6",
+                "seven", "7",
+                "eight", "8",
+                "nine","9");
 
-        int answer = Integer.parseInt(myStringArr[0]);
-        String operator = "";
-        for (int i=1; i<myStringArr.length; i++) {
-            if(myStringArr[i].equals("+") || myStringArr[i].equals("-")){
-                operator = myStringArr[i];
-                continue;
-            }
-            if(operator.equals("+")){
-                answer = answer + Integer.parseInt(myStringArr[i]);
-            }else if(operator.equals("-")){
-                answer = answer - Integer.parseInt(myStringArr[i]);
+
+        String numStr = "";
+        List<String> numList = new ArrayList<>();
+        for (int i=0; i<numbers.length(); i++){
+            numStr += numbers.charAt(i);
+            if(numbersMap.get(numStr) != null){
+                numList.add(numbersMap.get(numStr));
+                numStr = "";
             }
         }
+
+
+        String answer = String.join("", numList);
+        return Long.parseLong(answer); // 숫자가 너무 커서 Integer 로 하면 에러나는거였음...
+    }
+
+
+    // 그냥 replaceAll 하면 되는거였음을....
+    public static long solution_1(String numbers){
+        String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        // 인덱스 값 순서대로 zero ~ nine 까지
+        // numbers_arr[0] -> zero -> 인덱스값이랑 의미가 동일 저 값을 찾으면 인덱스 값을 String 으로 변환해서 replace 해줌.
+        for (int i = 0; i < numbers_arr.length; i++) {
+            numbers = numbers.replaceAll(numbers_arr[i], String.valueOf(i));
+        }
+
+        long answer = Long.parseLong(numbers);
         return answer;
     }
 
-    // 리팩토링 해봅시다.
-    public static int solution_1(String my_string){
-        String[] myStringArr = my_string.split(" ");
-        int answer = Integer.parseInt(myStringArr[0]); // 초기값. 맨 처음 숫자
 
-        for(int i=1; i<myStringArr.length; i += 2){ // 연산자 위치만 탐색. 연산자는 홀수 인덱스에 위치해 있는다. 그래서 1부터 시작해서 2씩 증감.
-            String operator = myStringArr[i];
-            int number = Integer.parseInt(myStringArr[i+1]); // 숫자는 연산자 다음 위치.
-            answer += operator.equals("+") ? number : -number;
-        }
-
-        return answer;
-    }
 }
 

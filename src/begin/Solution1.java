55f119f728c14341482ee9200135699c5e0b9620
pyojeyn <jeynpyo@gmail.com>
2024-12-09 17:17:17 +0900

    문자열 계산하기

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,61 +6,49 @@ import java.util.stream.IntStream;
 
 
 /*
-    [공 던지기]
-     머쓱이는 친구들과 동그랗게 서서 공 던지기 게임을 하고 있습니다.
-     공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 그다음 사람에게만 던질 수 있습니다.
-     친구들의 번호가 들어있는 정수 배열 numbers와 정수 K가 주어질 때,
-     k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 solution 함수를 완성해보세요.
-     numbers의 길이는 3부터 99까지.
-     k의 길이는 1부터 999까지.
+    [문자열 계산하기]
+     my_string은 "3 + 5"처럼 문자열로 된 수식입니다. 문자열 my_string이 매개변수로 주어질 때,
+     수식을 계산한 값을 return 하는 solution 함수를 완성해주세요.
  */
-// 5점 획득! 어려웠지만.. 어찌저찌 풀어서 5점 받았다.
+// 11점!!!!!!!
 public class Solution1 {
     public static void main(String[] args) {
-        // [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
-        // [1,2,3,1,2,3]
-        System.out.println(solution_1(new int[]{1,2,3,4}, 2));
-        System.out.println(solution_1(new int[]{1, 2, 3, 4, 5, 6}, 5));
-        System.out.println(solution_1(new int[]{1, 2, 3}, 3));
+        System.out.println(solution("3 + 5 - 2"));
+        System.out.println(solution_1("3 + 5 - 2"));
     }
 
 
-    public static int solution(int[] numbers, int k){
-        int answer = 0;
-        int sd = numbers.length / 2;
-        int[] numbersPlus = {};
+    public static int solution(String my_string){
+        String[] myStringArr = my_string.split(" ");
 
-        // 진짜 비효율적임 굳이 배열을 확장할 필요가 없었다..
-        // 괜찮어 배열 인덱스 순환할줄 몰라서 그런거니까...!!!
-        if(sd <= k){
-            numbersPlus = IntStream.range(0, k)
-                    .flatMap(i -> IntStream.of(numbers)) // 배열을 times 번 반복
-                    .toArray();// 최종 결과 배열로 변환
-        }
-
-        int cnt = 1;
-        for (int i=0; i<numbersPlus.length; i+=2){
-            if(cnt == k){
-                answer = numbersPlus[i];
-                break;
+        int answer = Integer.parseInt(myStringArr[0]);
+        String operator = "";
+        for (int i=1; i<myStringArr.length; i++) {
+            if(myStringArr[i].equals("+") || myStringArr[i].equals("-")){
+                operator = myStringArr[i];
+                continue;
             }
-            cnt++;
+            if(operator.equals("+")){
+                answer = answer + Integer.parseInt(myStringArr[i]);
+            }else if(operator.equals("-")){
+                answer = answer - Integer.parseInt(myStringArr[i]);
+            }
+        }
+        return answer;
+    }
+
+    // 리팩토링 해봅시다.
+    public static int solution_1(String my_string){
+        String[] myStringArr = my_string.split(" ");
+        int answer = Integer.parseInt(myStringArr[0]); // 초기값. 맨 처음 숫자
+
+        for(int i=1; i<myStringArr.length; i += 2){ // 연산자 위치만 탐색. 연산자는 홀수 인덱스에 위치해 있는다. 그래서 1부터 시작해서 2씩 증감.
+            String operator = myStringArr[i];
+            int number = Integer.parseInt(myStringArr[i+1]); // 숫자는 연산자 다음 위치.
+            answer += operator.equals("+") ? number : -number;
         }
 
         return answer;
     }
-
-    public static int solution_1(int[] numbers, int k){
-        // 2 % 4 = 2번 인덱스
-        // 8 % 6 = 2번 인덱스
-        // 4 % 3 = 1번 인덱스
-
-        int index = (2 * (k - 1)) % numbers.length;
-        /* k - 1: 공을 첫 번째로 던지는 사람은 이미 선택(맨첫번째)되었기 때문에, k-1번의 이동이 필요하다.
-           2 * (k - 1) : 한 번에 두 칸씩 건너뛰므로. 총 이동거리는 저렇게 된다.
-           % numbers.length : 나머지값이 인덱스가 됨. 나머지 연산으로 배열의 인덱스를 순환시킬 수 있음. */
-        return numbers[index];
-    }
-
-
 }
+

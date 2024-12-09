492df95f057ca37f028153dda35e5d3055092d68
pyojeyn <jeynpyo@gmail.com>
2024-12-09 18:34:27 +0900

    삼각형의 완성조건(2)

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,63 +6,44 @@ import java.util.stream.IntStream;
 
 
 /*
-    [영어가 싫어요]
-     영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다.
-     문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+    [삼각형의 완성조건(2)]
+     선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
+
+
  */
+// 2점
 
 
-// 4점.
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution("onetwothreefourfivesixseveneightnine"));
-        System.out.println(solution("onefourzerosixseven"));
+        System.out.println(solution(new int[]{3, 6}));
+        System.out.println(solution(new int[]{1, 2}));
+        System.out.println(solution(new int[]{11, 7}));
     }
 
 
-    public static long solution(String numbers){
-        Map<String, String> numbersMap = Map.of(
-                "zero","0",
-                "one", "1",
-                "two", "2",
-                "three", "3",
-                "four", "4",
-                "five", "5",
-                "six", "6",
-                "seven", "7",
-                "eight", "8",
-                "nine","9");
+    public static int solution(int[] numbers){
+        int answer = 0;
+        // 가장 긴 길이가 6일때.
+        int more = Arrays.stream(numbers).max().getAsInt() - Arrays.stream(numbers).min().getAsInt();
+//        int x = 0;
+//        x > 6 - 3 ;
+
+        // 가장 긴 길이가 나머지 한변 일때.
+        int less = numbers[0] + numbers[1];
+//        x < 6+3
 
 
-        String numStr = "";
-        List<String> numList = new ArrayList<>();
-        for (int i=0; i<numbers.length(); i++){
-            numStr += numbers.charAt(i);
-            if(numbersMap.get(numStr) != null){
-                numList.add(numbersMap.get(numStr));
-                numStr = "";
-            }
+        for(int i=more+1; i<less; i++){
+            answer++;
         }
-
-
-        String answer = String.join("", numList);
-        return Long.parseLong(answer); // 숫자가 너무 커서 Integer 로 하면 에러나는거였음...
+       return answer;
     }
 
 
-    // 그냥 replaceAll 하면 되는거였음을....
-    public static long solution_1(String numbers){
-        String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-        // 인덱스 값 순서대로 zero ~ nine 까지
-        // numbers_arr[0] -> zero -> 인덱스값이랑 의미가 동일 저 값을 찾으면 인덱스 값을 String 으로 변환해서 replace 해줌.
-        for (int i = 0; i < numbers_arr.length; i++) {
-            numbers = numbers.replaceAll(numbers_arr[i], String.valueOf(i));
-        }
-
-        long answer = Long.parseLong(numbers);
-        return answer;
-    }
 
 
 }

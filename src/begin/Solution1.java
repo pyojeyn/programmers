cded0f72cb6126b91279621d38b12ad90156c759
pyojeyn <jeynpyo@gmail.com>
2024-12-10 16:33:29 +0900

    [구슬을 나누는 경우의 수] 팩토리얼

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,45 +6,37 @@ import java.util.stream.IntStream;
 
 
 /*
-    [삼각형의 완성조건(2)]
-     선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
-가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
-삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
-
-
+    [구슬을 나누는 경우의 수]
+        balls -> n, share -> m
+        n! 나누기 (n-m)! * m!
+        팩토리얼 관련 문제....
  */
-// 2점
 
+// 11점..ㅎ
 
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{3, 6}));
-        System.out.println(solution(new int[]{1, 2}));
-        System.out.println(solution(new int[]{11, 7}));
+        System.out.println(solution(5,3));
+        System.out.println(solution(3,2));
+        System.out.println(solution(30,29));
     }
 
 
-    public static int solution(int[] numbers){
-        int answer = 0;
-        // 가장 긴 길이가 6일때.
-        int more = Arrays.stream(numbers).max().getAsInt() - Arrays.stream(numbers).min().getAsInt();
-//        int x = 0;
-//        x > 6 - 3 ;
+    public static int solution(int balls, int share){
 
-        // 가장 긴 길이가 나머지 한변 일때.
-        int less = numbers[0] + numbers[1];
-//        x < 6+3
+        if(balls == share) return 1;
 
+        // 조합 계산 최적화: 분자와 분모를 순차적으로 나눔
+        long result = 1;
 
-        for(int i=more+1; i<less; i++){
-            answer++;
+        // 결국 나누는 횟수는 share 개수만큼이니까 share만큼 for문
+       for (int i = 0; i < share; i++) {
+            System.out.println("i=" + i);
+            result *= (balls - i);  // 분자
+            result /= (i + 1); // 분모
         }
-       return answer;
+
+        return (int) result;
     }
-
-
-
-
-
 }
 

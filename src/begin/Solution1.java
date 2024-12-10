be715d96ed851f80fd3a6a4c602a5f68aa2fd8af
pyojeyn <jeynpyo@gmail.com>
2024-12-10 18:06:48 +0900

    2차원으로 만들기

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,32 +6,39 @@ import java.util.stream.IntStream;
 
 
 /*
-    [팩토리얼]
-    i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다.
-    예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다.
-    정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
+    [2차원으로 만들기]
+    정수 배열 num_list와 정수 n이 매개변수로 주어집니다.
+    num_list를 다음 설명과 같이 2차원 배열로 바꿔 return하도록 solution 함수를 완성해주세요.
+
+    num_list가 [1, 2, 3, 4, 5, 6, 7, 8] 로 길이가 8이고
+    n이 2이므로 num_list를 2 * 4 배열로 다음과 같이 변경합니다.
+    2차원으로 바꿀 때에는 num_list의 원소들을 앞에서부터 n개씩 나눠 2차원 배열로 변경합니다.
  */
-// 3점!! 이건 좀 뿌듯하네 ㅋ
+
+// 1점... ㅎ
 
 
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution(3628800));
-        System.out.println(solution(7));
-        System.out.println(solution(6));
-        System.out.println(solution(24));
-        System.out.println(solution(1));
-        System.out.println(solution(5));
+        System.out.println(Arrays.deepToString(solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 2)));
+        System.out.println(Arrays.deepToString(solution(new int[]{100, 95, 2, 4, 5, 6, 18, 33, 948}, 3)));
+
     }
 
 
-    public static int solution(int n){
-        int factorialNum = 1;
-        while (n > 1 && factorialNum < n){
-            factorialNum++;
-            n /= factorialNum;
+    public static int[][] solution(int[] num_list, int n){
+        int[][] answer = new int[num_list.length/n][n];
+
+        for (int i=0, index=0; i<num_list.length; i+=n, index++){
+            int[] fArr = new int[n];
+            for(int j=0; j<n; j++){
+                fArr[j] = num_list[j+i];
+                System.out.println("fArr["+j+"] : " + fArr[j]);
+            }
+            answer[index] = fArr;
+            System.out.println("answer= " + Arrays.deepToString(answer));
         }
-        return factorialNum;
+        return answer;
     }
 }
 

acb2810932c9a49e45b22a7582b8dc8f5a7ea26f
pyojeyn <jeynpyo@gmail.com>
2024-12-10 17:12:27 +0900

    팩토리얼

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,37 +6,32 @@ import java.util.stream.IntStream;
 
 
 /*
-    [구슬을 나누는 경우의 수]
-        balls -> n, share -> m
-        n! 나누기 (n-m)! * m!
-        팩토리얼 관련 문제....
+    [팩토리얼]
+    i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다.
+    예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다.
+    정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
  */
+// 3점!! 이건 좀 뿌듯하네 ㅋ
 
-// 11점..ㅎ
 
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution(5,3));
-        System.out.println(solution(3,2));
-        System.out.println(solution(30,29));
+        System.out.println(solution(3628800));
+        System.out.println(solution(7));
+        System.out.println(solution(6));
+        System.out.println(solution(24));
+        System.out.println(solution(1));
+        System.out.println(solution(5));
     }
 
 
-    public static int solution(int balls, int share){
-
-        if(balls == share) return 1;
-
-        // 조합 계산 최적화: 분자와 분모를 순차적으로 나눔
-        long result = 1;
-
-        // 결국 나누는 횟수는 share 개수만큼이니까 share만큼 for문
-       for (int i = 0; i < share; i++) {
-            System.out.println("i=" + i);
-            result *= (balls - i);  // 분자
-            result /= (i + 1); // 분모
+    public static int solution(int n){
+        int factorialNum = 1;
+        while (n > 1 && factorialNum < n){
+            factorialNum++;
+            n /= factorialNum;
         }
-
-        return (int) result;
+        return factorialNum;
     }
 }
 

e2cac353f90c413d50a10993aa41f58e657ea000
pyojeyn <jeynpyo@gmail.com>
2024-12-11 21:09:21 +0900

    외계어 사전

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,38 +6,41 @@ import java.util.stream.IntStream;
 
 
 /*
-    [2차원으로 만들기]
-    정수 배열 num_list와 정수 n이 매개변수로 주어집니다.
-    num_list를 다음 설명과 같이 2차원 배열로 바꿔 return하도록 solution 함수를 완성해주세요.
-
-    num_list가 [1, 2, 3, 4, 5, 6, 7, 8] 로 길이가 8이고
-    n이 2이므로 num_list를 2 * 4 배열로 다음과 같이 변경합니다.
-    2차원으로 바꿀 때에는 num_list의 원소들을 앞에서부터 n개씩 나눠 2차원 배열로 변경합니다.
+    [외계어 사전]
+PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다.
+알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다.
+spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
  */
 
-// 1점... ㅎ
-
-
+// 2점!
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 2)));
-        System.out.println(Arrays.deepToString(solution(new int[]{100, 95, 2, 4, 5, 6, 18, 33, 948}, 3)));
 
+        System.out.println(solution(new String[]{"p", "o", "s"}, new String[]{"sod", "eocd", "qixm", "adio", "soo"}));
+        System.out.println(solution(new String[]{"z", "d", "x"}, new String[]{"def", "dww", "dzx", "loveaw"}));
+        System.out.println(solution(new String[]{"s", "o", "m", "d"}, new String[]{"moos", "dzx", "smm", "sunmmo", "som"}));
     }
 
 
-    public static int[][] solution(int[] num_list, int n){
-        int[][] answer = new int[num_list.length/n][n];
+    public static int solution(String[] spell, String[] dic){
+        int answer = 2;
 
-        for (int i=0, index=0; i<num_list.length; i+=n, index++){
-            int[] fArr = new int[n];
-            for(int j=0; j<n; j++){
-                fArr[j] = num_list[j+i];
-                System.out.println("fArr["+j+"] : " + fArr[j]);
+        int lengthOfDic = dic.length;
+
+
+        for (int i=0; i<lengthOfDic; i++){
+            int cnt = 0;
+            for (String s : spell) {
+                char spellJ = s.charAt(0);
+                int count = (int) dic[i].chars().filter(c -> c == spellJ).count();
+                if(count == 1){
+                    cnt += count;
+                }
             }
-            answer[index] = fArr;
-            System.out.println("answer= " + Arrays.deepToString(answer));
+            if(cnt == spell.length) answer = 1;
         }
+
+
         return answer;
     }
 }

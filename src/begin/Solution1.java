3d52f9d4636d1fb2bb8c37cce15760c6864eebe5
pyojeyn <jeynpyo@gmail.com>
2024-12-12 11:46:55 +0900

    캐릭터의 좌표

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,38 +6,69 @@ import java.util.stream.IntStream;
 
 
 /*
-    [외계어 사전]
-PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다.
-알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다.
-spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
+    [캐릭터의 좌표]
+머쓱이는 RPG게임을 하고 있습니다.
+게임에는 up, down, left, right 방향키가 있으며
+각 키를 누르면 위, 아래, 왼쪽, 오른쪽으로 한 칸씩 이동합니다.
+예를 들어 [0,0]에서 up을 누른다면 캐릭터의 좌표는 [0, 1], down을 누른다면 [0, -1],
+left를 누른다면 [-1, 0], right를 누른다면 [1, 0]입니다.
+머쓱이가 입력한 방향키의 배열 keyinput와 맵의 크기 board이 매개변수로 주어집니다.
+캐릭터는 항상 [0,0]에서 시작할 때 키 입력이 모두 끝난 뒤에 캐릭터의 좌표 [x, y]를 return하도록 solution 함수를 완성해주세요.
  */
 
-// 2점!
+
 public class Solution1 {
     public static void main(String[] args) {
+//        System.out.println(Arrays.toString(solution(new String[]{"left", "right", "up", "right", "right"}, new int[]{11, 11})));
+//        System.out.println(Arrays.toString(solution(new String[]{"down", "down", "down", "down", "down"}, new int[]{7, 9})));
 
-        System.out.println(solution(new String[]{"p", "o", "s"}, new String[]{"sod", "eocd", "qixm", "adio", "soo"}));
-        System.out.println(solution(new String[]{"z", "d", "x"}, new String[]{"def", "dww", "dzx", "loveaw"}));
-        System.out.println(solution(new String[]{"s", "o", "m", "d"}, new String[]{"moos", "dzx", "smm", "sunmmo", "som"}));
+
     }
 
 
-    public static int solution(String[] spell, String[] dic){
-        int answer = 2;
 
-        int lengthOfDic = dic.length;
+    // 비슷했는데... 아깝다.
+    /*
+    * 핵심 1 : 배열 원소의 값 바로 변경하지 말고 임시 변수 사용.
+    * 핵심 2 : 경계 조건을 확인한 후, 유효한 경우에만 배열 원소를 업데이트 한다.
+    * */
+    public static int[] solution(String[] keyinput, int[] board){
+        int[] answer = {0,0};
+        int maxHeight = board[1] / 2;
+        int maxWidth = board[0] / 2;
+        // -- 여기까진 맞음...
 
+        for(int i=0; i< keyinput.length; i++){
 
-        for (int i=0; i<lengthOfDic; i++){
-            int cnt = 0;
-            for (String s : spell) {
-                char spellJ = s.charAt(0);
-                int count = (int) dic[i].chars().filter(c -> c == spellJ).count();
-                if(count == 1){
-                    cnt += count;
-                }
+            /* 캐치 못했던 점1.
+             answer 배열의 값을 바로 변경하는 대신, 새로운 int 변수를 사용하여 임시로 좌표 계산. */
+            int newX = answer[0]; // 가로 좌표
+            int newY = answer[1]; // 세로 좌표
+
+            switch (keyinput[i]){
+                case "left":
+                    newX -= 1;
+                    break;
+                case "right":
+                    newX += 1;
+                    break;
+                case "up":
+                    newY += 1;
+                    break;
+                case "down":
+                    newY -= 1;
             }
-            if(cnt == spell.length) answer = 1;
+
+
+            /* 캐치 못했던 점2.
+             for문 안에서 최종 값 처리! 유효한 입력일 경우에만 answer배열의 값을 업데이트.
+             무효한 입력은 아예 반영되지 않음. */
+            if (newX >= -maxWidth && newX <= maxWidth) {
+                answer[0] = newX;
+            }
+            if (newY >= -maxHeight && newY <= maxHeight) {
+                answer[1] = newY;
+            }
         }
 
 

bd2d48536f3ccf7f5a7f836f97b0b3b1ee81a14a
pyojeyn <jeynpyo@gmail.com>
2024-12-12 23:18:13 +0900

    로그인 성공?

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,49 +6,49 @@ import java.util.stream.IntStream;
 
 
 /*
-    [직사각형 넓이 구하기]
-2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다.
-직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
-가 담겨있는 배열 dots가 매개변수로 주어질 때,
-직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
+    [로그인 성공?]
+머쓱이는 프로그래머스에 로그인하려고 합니다.
+머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때,
+다음과 같이 로그인 성공, 실패에 따른 메시지를 return 하도록 solution 함수를 완성해주세요.
  */
 
-// 직사각형 5점 !
+// 4점!
+
 
 
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution(new int[][]{{1,1}, {2,1}, {2,2}, {1,2}}));
-        System.out.println(solution(new int[][]{{-1,-1}, {1,1}, {1,-1}, {-1,1}}));
+        System.out.println(solution(new String[]{"meosseugi", "1234"},
+                new String[][]{{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "1234"}}));
+        System.out.println(solution(new String[]{"programmer01", "15789"},
+                new String[][]{{"programmer02", "111111"}, {"programmer00", "134"}, {"programmer01", "1145"}}));
+
+        System.out.println(solution(new String[]{"rabbit04", "98761"},
+                new String[][]{{"jaja11", "98761"}, {"krong0313", "29440"}, {"rabbit00", "111333"}}));
     }
 
 
 
 
-    public static int solution(int[][] dots){
-        /* 가로 길이 : x좌표의 최대값 - 최소값을 구하고 뺌
-           세로 길이 : y좌표의 최대값 - 최소값을 구하고 뺌 */
+    public static String solution(String[] id_pw, String[][] db){
 
-        int maxX = dots[0][0]; // 1
-        int minX = dots[0][0]; // 1
-        int maxY = dots[0][1]; // 1
-        int minY = dots[0][1]; // 1
-        for(int i=1; i< dots.length; i++){
-            for (int j=0; j<dots[i].length; j++){
-               int current = dots[i][j];
-                System.out.println("current = " + current);
-               if (j == 0){
-                   maxX = Math.max(maxX, current);
-                   minX = Math.min(minX, current);
-               }else{
-                   maxY = Math.max(maxY, current);
-                   minY = Math.min(minY, current);
-               }
+        String answer = "fail";
+
+        String id = id_pw[0];
+        String pw = id_pw[1];
+        for (String[] member : db) {
+            if(member[0].equals(id)){
+                /* 제한사항에 회원들의 아이디는 같을 수 없기 때문에 같이 for문에서 조건문으로 명시 가능.
+                 한번 이 if 문 들어오면 두번다시 이 안에 들어올일 없음.
+                 이 if문에 한번 들어오면 끝인거임. 무조건 둘중에 하나임. */
+
+                /* 문제의 제한사항에 따르면 회원들의 아이디는 중복될 수 없기 때문에,
+                   for 루프 내에서 아이디가 일치하는 경우에는 그 아이디를 가진 회원이 단 하나만 존재. */
+                if (member[1].equals(pw)) answer = "login";
+                else answer = "wrong pw";
             }
         }
-
-
-        return (maxX - minX) * (maxY - minY);
+        return answer;
     }
 }
 

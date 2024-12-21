9db2fdccb113959ed1a1311e37581bc0413bbe9f
pyojeyn <jeynpyo@gmail.com>
2024-12-21 12:01:59 +0900

    등수 매기기

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,58 +6,134 @@ import java.util.stream.IntStream;
 
 
 /*
-    [저주의 숫자 3]
-3x 마을 사람들은 3을 저주의 숫자라고 생각하기 때문에 3의 배수와 숫자 3을 사용하지 않습니다. 3x 마을 사람들의 숫자는 다음과 같습니다.
-정수 n이 매개변수로 주어질 때, n을 3x 마을에서 사용하는 숫자로 바꿔 return하도록 solution 함수를 완성해주세요.
+    [등수 매기기]
+영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다.
+영어 점수와 수학 점수를 담은 2차원 정수 배열 score 가 주어질 때,
+영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return 하도록 solution 함수를 완성해주세요.
  */
 
-// 5점!!
-
+// 8점.
 
 
 
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution_1(15));
-        System.out.println(solution_1(40));
+
+        System.out.println(Arrays.toString(solution(new int[][]{{100, 0}, {0, 100}, {50, 50}})));
+        System.out.println(Arrays.toString(solution(new int[][]{{80, 70}, {90, 50}, {40, 70}, {50, 80}})));
+        System.out.println(Arrays.toString(solution(new int[][]{{80, 70}, {70, 80}, {30, 50}, {90, 100}, {100, 90}, {100,100}, {10,30}})));
+
+
+
+        /*
+           ***내가 계속 틀렸던 이유***
+           코드에 문제가 있나 싶었지만. 타입 문제였다.
+           [90, 91] 의 평균은 90.5 이다. 그럼 [90, 90] 의 평균인 90 점보다 등수가 높아야 하는게 맞다.
+           그러므로 타입을 Double 로 선언해야 한다.
+           하지만 타입을 Integer 로 선언하면 둘의 평균이 90점이 되어 등수가 동일하게 된다.
+        */
+        System.out.println(Arrays.toString(solution_wrong(new int[][]{{90,91}, {90,90}, {30,90}}))); // -> [1, 1, 3]
+        System.out.println(Arrays.toString(solution(new int[][]{{90,91}, {90,90}, {30,90}}))); // -> [1, 2, 3] 이게 마따..
+
+
+        System.out.println("---Double & Int---");
+        double value = 90.9;
+        int truncatedValue = (int) value;
+        System.out.println("value = " + value);
+        System.out.println("truncatedValue = " + truncatedValue);
+
     }
 
 
-    public static int solution(int n){
-        Map<Integer, Integer> number3xMap = new HashMap<>();
-        // i: 10진숫자, j: 3x마을 숫자
-        int i = 1;
-        int j = 1;
-        while (i < 101){
 
-            while ((j % 3 == 0 || String.valueOf(j).contains("3"))){
-                j++;
-            }
-            number3xMap.put(i, j);
-            i++;
-            j++;
+
+
+
+    public static int[] solution(int[][] score){
+        System.out.println("Hello! Here is the correct solution code!!");
+
+        // 내림차순으로 정렬할 배열
+        Double[] answer = new Double[score.length];
+
+        // 그대로 순서를 보존할 배열
+        Double[] finalAnswer = new Double[score.length];
+
+        // 내림차순할 배열, 순서보존 배열 각각 평균값을 계산해주어 넣는다
+        for (int i=0; i< score.length; i++){
+            double avg = (score[i][0] + score[i][1]) / 2.0;
+            answer[i] = avg;
+            finalAnswer[i] = avg;
         }
-        return number3xMap.get(n);
-    }
 
-    // 개선한 코드.
-    public static int solution_1(int n) {
-        Map<Integer, Integer> number3xMap = new HashMap<>();
-        int i = 1;
-        int j = 1;
+        // 내림차순으로 배열 정렬
+        Arrays.sort(answer, Collections.reverseOrder());
 
-        while (i <= n) { // 제한을 n으로 조정
-            String jStr = String.valueOf(j);
-            while (j % 3 == 0 || jStr.contains("3")) {
-                j++;
-                jStr = String.valueOf(j); // j가 증가할 때마다 jStr 업데이트
+        // 평균점수와 등수를 저장할 Map 생성
+        Map<Double, Integer> rankMap = new HashMap<>();
+
+        // **rank 는 1부터 시작해서 1씩 증가하기 때문에 배열을 내림차순으로 정렬해야 한다.
+        int rank = 1;
+        for(int i=0; i<answer.length; i++){
+            if(!rankMap.containsKey(answer[i])){
+                rankMap.put(answer[i],  rank++);
+            }else{
+                rankMap.put(answer[i], rankMap.get(answer[i]));
+                rank++;
             }
-            number3xMap.put(i, j);
-            i++;
-            j++;
         }
 
-        return number3xMap.get(n);
+        System.out.println(rankMap);
+
+        int[] ranks = new int[answer.length];
+        for(int i=0; i<finalAnswer.length; i++){
+            ranks[i] = rankMap.get(finalAnswer[i]);
+        }
+
+
+        return ranks;
     }
+
+
+
+
+    // 타입 원인으로 틀린 정답.
+    public static int[] solution_wrong(int[][] score) {
+        System.out.println("Hello! Here is the wrong solution code!!");
+        Integer[] answer = new Integer[score.length];
+        Integer[] finalAnswer = new Integer[score.length];
+
+        for (int i=0; i< score.length; i++){
+            int avg = (score[i][0] + score[i][1]) / 2;
+            answer[i] = avg;
+            finalAnswer[i] = avg;
+        }
+
+        Arrays.sort(answer, Collections.reverseOrder());
+
+        Map<Integer, Integer> rankMap = new HashMap<>();
+
+        int rank = 1;
+        for(int i=0; i<answer.length; i++){
+            if(!rankMap.containsKey(answer[i])){
+                rankMap.put(answer[i], rank++);
+            }else{
+                rankMap.put(answer[i], rankMap.get(answer[i]));
+                rank++;
+            }
+        }
+
+        System.out.println(rankMap);
+
+        int[] ranks = new int[answer.length];
+        for(int i=0; i<finalAnswer.length; i++){
+            ranks[i] = rankMap.get(finalAnswer[i]);
+        }
+
+
+        return ranks;
+    }
+
+
+
 }
 

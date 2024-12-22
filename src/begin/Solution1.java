b5665d9da45a66fc919891e2965b158425e5699b
pyojeyn <jeynpyo@gmail.com>
2024-12-22 14:40:16 +0900

    [유한 소수 판별하기] - 최대공약수,소인소분해 - 유클리드호제법 사용

--- src/begin/Solution1.java
+++ src/begin/Solution1.java
@@ -6,51 +6,63 @@ import java.util.stream.IntStream;
 
 
 /*
-    [치킨 쿠폰]
-프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다.
-쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고,
-서비스 치킨에도 쿠폰이 발급됩니다.
-시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+    [유한 소수 판별하기]
+소수점 아래 숫자가 계속되지 않고 유한개인 소수를 유한소수라고 합니다.
+분수를 소수로 고칠 때 유한소수로 나타낼 수 있는 분수인지 판별하려고 합니다.
+유한소수가 되기 위한 분수의 조건은 다음과 같습니다.
+    -> 기약분수로 나타내었을 때, 분모의 소인수가 2와 5만 존재해야 합니다.
+두 정수 a와 b가 매개변수로 주어질 때, a/b가 유한소수이면 1을, 무한소수라면 2를 return하도록 solution 함수를 완성해주세요.
  */
 
-// 4점!!!
 
-
-
+// 10점!!
 public class Solution1 {
     public static void main(String[] args) {
-        System.out.println(solution(100));
-        System.out.println(solution(1081));
+        System.out.println(solution(12, 21));
+        System.out.println(getGCD(12, 16));
+        System.out.println(primeFactorization(24));
     }
 
 
-    public static int solution(int chicken){
+    public static int solution(int a, int b){
+        int answer = 0;
+        /* 최대공약수 구하기 -> 유클리드 호제법 */
+        int gcd = getGCD(a, b);
+        b /= gcd;
 
-        /* 누적 서비스 치킨의 수를 저장할 변수 초기화 */
-        int serviceChicken = 0;
+        /* 소인수분해하기 */
+        List<Integer> sosus = primeFactorization(b);
 
-        /* 쿠폰은 총 시켜먹은 치킨의 수로 초기화 한다. */
-        int coupon = chicken;
+        /* 소수들 중 2와 5 이외의 숫자가 있는지에 대한 여부. */
+        boolean existsOtherNumbers = sosus.stream().anyMatch(num -> num != 2 && num != 5);
+        answer = existsOtherNumbers ? 2 : 1;
+        return answer;
+    }
 
-        /* 쿠폰이 10개 이상일 경우 계속해서 서비스 치킨을 받음 */
-        while (coupon > 9){
+    /* 유클리드 호제법 */
+    // 두 수를 나눗셈 반복하여 나머지가 0이 될 때까지 계산 -> 마지막으로 나머지가 0이 되었을 때의 나누는 수가 최대공약수가 된다.
+    public static int getGCD(int a, int b){
+        while (b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 
-            /* 쿠폰이 10개 이상이면, 10개당 1마리의 치킨을 받을 수 있음 */
-            int newChicken = coupon / 10;
-
-            /* 서비스 치킨을 받은 후 남은 쿠폰은 서비스 받은 치킨으로 받은 쿠폰과 기존 쿠폰의 나머지를 합친 값 */
-            coupon = newChicken + (coupon % 10);
-
-            /* 이번에 받은 서비스 치킨의 수를 누적 */
-            serviceChicken += newChicken;
-
-            System.out.println("누적 서비스 치킨: " + serviceChicken + ", 남은 쿠폰: " + coupon); // 디버깅 출력
+    /* 소인수분해 */
+    public static List<Integer> primeFactorization(int b){
+        List<Integer> sosus = new ArrayList<>();
+        for (int i=2; i<=b; i++){
+            if(b % i == 0){
+                while (b % i == 0){
+                    b /= i; // i로 나눌 수 있을 때까지 나눔
+                }
+                sosus.add(i);
+            }
         }
 
-
-
-        return serviceChicken;
-
+        return sosus;
     }
 
 
